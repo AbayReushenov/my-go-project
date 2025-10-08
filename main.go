f38b7c59e27fcf9d60d1394df16d2aa429e9c0ed
AbayReushenov <abay.reushenov@gmail.com>
2025-10-08 11:41:15 +0300

    Lesson Six : Error. Exported names In Go, a name is exported if it begins with a capital letter.

--- main.go
+++ main.go
@@ -6,10 +6,7 @@ import (
 )
 
 func main() {
-	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	fmt.Println(math.pi)
 }
-// aaaaa@aaaaa-GF63-Thin-9SCXR:~/my-go-project$ go build main.go
-// aaaaa@aaaaa-GF63-Thin-9SCXR:~/my-go-project$ ./main
-// Now you have 2.6457513110645907 problems.
-// aaaaa@aaaaa-GF63-Thin-9SCXR:~/my-go-project$ go run main.go
-// Now you have 2.6457513110645907 problems.
+// # command-line-arguments
+// ./main.go:9:19: undefined: math.pi
