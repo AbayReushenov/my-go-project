6c24f3584b720f397d83c625b407a1e5637bb8a0
AbayReushenov <abay.reushenov@gmail.com>
2025-10-08 13:15:06 +0300

    Lesson 10.2: Переменные и их инициализация. Короткое присваивание внутри функции

--- main.go
+++ main.go
@@ -2,14 +2,10 @@ package main
 
 import "fmt"
 
-var i, j int = 1, 2
-
 func main() {
-	var c, python, java = true, false, "no!"
-	fmt.Println(i, j, c, python, java)
+	var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+
+	fmt.Println(i, j, k, c, python, java) // 1 2 3 true false no!
 }
-
-//  Variables with initializers
-// A var declaration can include initializers, one per variable.
-
-// If an initializer is present, the type can be omitted; the variable will take the type of the initializer.
