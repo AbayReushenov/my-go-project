7ddb3ba80d2a9dfdfe542b1872aa98d5e44b938d
AbayReushenov <abay.reushenov@gmail.com>
2025-10-08 13:22:53 +0300

    Lesson 11: Основные типы переменных

--- main.go
+++ main.go
@@ -1,11 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+// Go's basic types are
+
+// bool
+
+// string
+
+// int  int8  int16  int32  int64
+// uint uint8 uint16 uint32 uint64 uintptr
+
+// byte // alias for uint8
+
+// rune // alias for int32
+//      // represents a Unicode code point
+
+// float32 float64
+
+// complex64 complex128
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
 
 func main() {
-	var i, j int = 1, 2
-	k := 3
-	c, python, java := true, false, "no!"
-
-	fmt.Println(i, j, k, c, python, java) // 1 2 3 true false no!
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
 }
+
+// Type: bool Value: false
+// Type: uint64 Value: 18446744073709551615
+// Type: complex128 Value: (2+3i)
