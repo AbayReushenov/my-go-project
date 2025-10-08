52825de9ded5c65d6f564b07adee8ce8351fafee
AbayReushenov <abay.reushenov@gmail.com>
2025-10-08 13:26:02 +0300

    Lesson 12: Переменные без инициализации получают нулевые значения

--- main.go
+++ main.go
@@ -1,39 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"math/cmplx"
-)
-
-// Go's basic types are
-
-// bool
-
-// string
-
-// int  int8  int16  int32  int64
-// uint uint8 uint16 uint32 uint64 uintptr
-
-// byte // alias for uint8
-
-// rune // alias for int32
-//      // represents a Unicode code point
-
-// float32 float64
-
-// complex64 complex128
-var (
-	ToBe   bool       = false
-	MaxInt uint64     = 1<<64 - 1
-	z      complex128 = cmplx.Sqrt(-5 + 12i)
-)
+import "fmt"
 
 func main() {
-	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
-	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
-	fmt.Printf("Type: %T Value: %v\n", z, z)
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
 }
+// 0 0 false ""
 
-// Type: bool Value: false
-// Type: uint64 Value: 18446744073709551615
-// Type: complex128 Value: (2+3i)
+
+// Variables declared without an explicit initial value are given their zero value.
+
+// The zero value is:
+
+// 0 for numeric types,
+// false for the boolean type, and
+// "" (the empty string) for strings.
