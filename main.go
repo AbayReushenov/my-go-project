b94dc70bed4c50a3c9a83b95e4d76f7d3a4b7d42
AbayReushenov <abay.reushenov@gmail.com>
2025-10-08 11:35:12 +0300

    Lesson Five: Math package

--- main.go
+++ main.go
@@ -2,22 +2,9 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"math"
 )
 
 func main() {
-	fmt.Println("My favorite number is", rand.Intn(10))
-    // By convention, the package name is the same as the last element of the import path.
-    // For instance, the "math/rand" package comprises files that begin with the statement
-    // package rand.
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
 }
-// aaaaa@aaaaa-GF63-Thin-9SCXR:~/my-go-project$ go run main.go
-// My favorite number is 4
-// aaaaa@aaaaa-GF63-Thin-9SCXR:~/my-go-project$ go run main.go
-// My favorite number is 6
-// aaaaa@aaaaa-GF63-Thin-9SCXR:~/my-go-project$ go build main.go
-// aaaaa@aaaaa-GF63-Thin-9SCXR:~/my-go-project$ ./main
-// My favorite number is 9
-// aaaaa@aaaaa-GF63-Thin-9SCXR:~/my-go-project$ ./main
-// My favorite number is 7
-// aaaaa@aaaaa-GF63-Thin-9SCXR:~/my-go-project$
