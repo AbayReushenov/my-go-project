dde0b38e654865c71af0f85221d6630910ad5293
AbayReushenov <abay.reushenov@gmail.com>
2025-10-08 10:22:37 +0300

    Lesson Two: Time functions

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is", time.Now())
 }
+
+// aaaaa@aaaaa-GF63-Thin-9SCXR:~/my-go-project$ ./main
+// Hello, World!
+// aaaaa@aaaaa-GF63-Thin-9SCXR:~/my-go-project$ go build main.go
+// aaaaa@aaaaa-GF63-Thin-9SCXR:~/my-go-project$ ./main
+// Welcome to the playground!
+// The time is 2025-10-08 10:19:38.253886962 +0300 MSK m=+0.000220249
+// aaaaa@aaaaa-GF63-Thin-9SCXR:~/my-go-project$ go run main.go
+// Welcome to the playground!
+// The time is 2025-10-08 10:20:19.063815585 +0300 MSK m=+0.000106998
+// aaaaa@aaaaa-GF63-Thin-9SCXR:~/my-go-project$
