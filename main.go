35a7f652220206cef6207e41b89312d141c230a1
AbayReushenov <abay.reushenov@gmail.com>
2025-10-08 11:32:21 +0300

    Lesson Four. Version 2: Математические функции. Импорт и использование.

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 
 func main() {
 	fmt.Println("My favorite number is", rand.Intn(10))
+    // By convention, the package name is the same as the last element of the import path.
+    // For instance, the "math/rand" package comprises files that begin with the statement
+    // package rand.
 }
 // aaaaa@aaaaa-GF63-Thin-9SCXR:~/my-go-project$ go run main.go
 // My favorite number is 4
