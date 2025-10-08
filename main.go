fd98c9386dbe8d9d2325dafc5e0214d05c8bf763
AbayReushenov <abay.reushenov@gmail.com>
2025-10-08 14:53:39 +0300

    Lesson 15: Константы

--- main.go
+++ main.go
@@ -1,28 +1,23 @@
+// Константы
+// Константы объявляются как переменные, но с ключевым словом const.
+
+// Константы могут иметь символьные, строковые, логические или числовые значения.
+
+// Константы нельзя объявлять с использованием синтаксиса :=.
 package main
 
 import "fmt"
 
+const Pi = 3.14
+
 func main() {
-    var i int
-    j := i // j is an int
-    fmt.Printf("i is of type %T\n", i)
-    fmt.Printf("j is of type %T\n", j)
+	const World = "世界"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
 
-    ii := 42           // int
-    f := 3.142        // float64
-    g := 0.867 + 0.5i // complex128
-
-    fmt.Printf("ii is of type %T\n", ii)
-    fmt.Printf("f is of type %T\n", f)
-    fmt.Printf("g is of type %T\n", g)
-
-	v := 42 // change me!
-	fmt.Printf("v is of type %T\n", v)
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
 }
-
-// i is of type int
-// j is of type int
-// ii is of type int
-// f is of type float64
-// g is of type complex128
-// v is of type int
+// Hello 世界
+// Happy 3.14 Day
+// Go rules? true
