5b49d762e38c7a47ba9c668d3911e57672850016
AbayReushenov <abay.reushenov@gmail.com>
2025-10-08 11:36:40 +0300

    Lesson Five/ Version 2: Math package

--- main.go
+++ main.go
@@ -8,3 +8,8 @@ import (
 func main() {
 	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
 }
+// aaaaa@aaaaa-GF63-Thin-9SCXR:~/my-go-project$ go build main.go
+// aaaaa@aaaaa-GF63-Thin-9SCXR:~/my-go-project$ ./main
+// Now you have 2.6457513110645907 problems.
+// aaaaa@aaaaa-GF63-Thin-9SCXR:~/my-go-project$ go run main.go
+// Now you have 2.6457513110645907 problems.
