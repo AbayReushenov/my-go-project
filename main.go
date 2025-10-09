30b84291e9ec14f5550a5fda7fa088bd03501567
AbayReushenov <abay.reushenov@gmail.com>
2025-10-09 18:25:25 +0300

    Lesson A struct is a collection of fields.

--- main.go
+++ main.go
@@ -2,37 +2,11 @@ package main
 
 import "fmt"
 
-func main() {
-	i, j := 42, 2701
-
-	p := &i         // point to i
-	fmt.Println(*p) // read i through the pointer
-	// 42
-	*p = 21         // set i through the pointer
-	fmt.Println(i)  // see the new value of i
-	// 21
-
-	p = &j         // point to j
-	*p = *p / 37   // divide j through the pointer
-	fmt.Println(j) // see the new value of j
-	// 73
+type Vertex struct {
+	X int
+	Y int
 }
 
-
-// Указатели
-// В Go есть указатели. Указатель хранит адрес памяти значения.
-
-// Тип *T — это указатель на значение T. Его нулевое значение — nil.
-
-// var p *int
-// Оператор & генерирует указатель на свой операнд.
-
-// i := 42
-// p = &i
-// Оператор * обозначает базовое значение указателя.
-
-// fmt.Println(*p) // прочитать i по указателю p
-// *p = 21 // установить i по указателю p
-// Это называется «разыменованием» или «косвенным обращением».
-
-// В отличие от C, в Go нет арифметики указателей.
+func main() {
+	fmt.Println(Vertex{1, 2}) // {1 2}
+}
