927e5cee7c82e6af80ceabe5038d4649baf86e4b
AbayReushenov <abay.reushenov@gmail.com>
2025-10-09 16:24:14 +0300

    Lesson For: As WHILE

--- main.go
+++ main.go
@@ -3,40 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	sum := 0
-	fmt.Println(sum)
-
-	for i := range 10 {
-			fmt.Println(i, sum)
-
-		sum += i
-
-		fmt.Println(i, sum)
+	sum := 1
+	for sum < 1030 { // As WHILE
+		sum += sum
 	}
-
-	fmt.Println(sum)
+	fmt.Println(sum) // 2048
 }
-
-// aaaaa@aaaaa-GF63-Thin-9SCXR:~/my-go-project$ go run main.go
-// 0
-// 0 0
-// 0 0
-// 1 0
-// 1 1
-// 2 1
-// 2 3
-// 3 3
-// 3 6
-// 4 6
-// 4 10
-// 5 10
-// 5 15
-// 6 15
-// 6 21
-// 7 21
-// 7 28
-// 8 28
-// 8 36
-// 9 36
-// 9 45
-// 45
