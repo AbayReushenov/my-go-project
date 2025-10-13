94a44c78bf81f00493edf39301b2023a45cde23d
AbayReushenov <abay.reushenov@gmail.com>
2025-10-13 17:09:22 +0300

    Slice can contain any type

--- main.go
+++ main.go
@@ -1,42 +1,30 @@
+// Следующий 14/27
+// https://go.dev/tour/moretypes/14
+// Slices of slices
+// Slices can contain any type, including other slices.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	a := make([]int, 5)
-	printSlice("a", a)
+	// Create a tic-tac-toe board.
+	board := [][]string{
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+	}
 
-	b := make([]int, 0, 5)
-	printSlice("b", b)
+	// The players take turns.
+	board[0][0] = "X"
+	board[2][2] = "O"
+	board[1][2] = "X"
+	board[1][0] = "O"
+	board[0][2] = "X"
 
-	c := b[:2]
-	printSlice("c", c)
-
-	d := c[2:5]
-	printSlice("d", d)
-
-
-// a len=5 cap=5 [0 0 0 0 0]
-// b len=0 cap=5 []
-// c len=2 cap=5 [0 0]
-// d len=3 cap=3 [0 0 0]
+	for i := 0; i < len(board); i++ {
+		fmt.Printf("%s\n", strings.Join(board[i], " "))
+	}
 }
-
-func printSlice(s string, x []int) {
-	fmt.Printf("%s len=%d cap=%d %v\n",
-		s, len(x), cap(x), x)
-}
-// Создание среза с помощью make
-// Срезы можно создавать с помощью встроенной функции make; именно так создаются массивы с динамическим размером.
-
-// Функция make выделяет обнулённый массив и возвращает срез, ссылающийся на этот массив:
-
-// a := make([]int, 5) // len(a)=5
-// Чтобы указать ёмкость, передайте make третий аргумент:
-
-// b := make([]int, 0, 5) // len(b)=0, cap(b)=5
-
-// b = b[:cap(b)] // len(b)=5, cap(b)=5
-// b = b[1:] // len(b)=4, cap(b)=4
-
-
