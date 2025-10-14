850241d703928b8745ed26fe164135f37127841b
AbayReushenov <abay.reushenov@gmail.com>
2025-10-14 16:54:55 +0300

    Slices. Многомерные срезы

--- main.go
+++ main.go
@@ -25,6 +25,6 @@ func main() {
 	board[0][2] = "X"
 
 	for i := 0; i < len(board); i++ {
-		fmt.Printf("%s\n", strings.Join(board[i], " "))
+		fmt.Printf("%s\n", strings.Join(board[i], "  "))
 	}
 }
