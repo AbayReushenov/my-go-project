8850c891c161707f80032da656717ed559e5e9ad
AbayReushenov <abay.reushenov@gmail.com>
2025-10-26 16:50:17 +0300

    Числовые константы

--- main.go
+++ main.go
@@ -2,25 +2,24 @@ package main
 
 import "fmt"
 
-const Pi = 3.14
+const (
+	// Create a huge number by shifting a 1 bit left 100 places.
+	// In other words, the binary number that is 1 followed by 100 zeroes.
+	Big = 1 << 100
+	// Shift it right again 99 places, so we end up with 1<<1, or 2.
+	Small = Big >> 99
+)
 
-func main() {
-	const World = "世界"
-	fmt.Println("Hello", World)
-	fmt.Println("Happy", Pi, "Day")
-
-	const Truth = true
-	fmt.Println("Go rules?", Truth)
+func needInt(x int) int { return x*10 + 1 }
+func needFloat(x float64) float64 {
+	return x * 0.1
 }
 
-// aaaaa@aaaaa-GF63-Thin-9SCXR:~/my-go-project$ go run main.go
-// Hello 世界
-// Happy 3.14 Day
-// Go rules? true
-
-// Константы
-// Константы объявляются как переменные, но с ключевым словом const.
-
-// Константы могут иметь символьные, строковые, логические или числовые значения.
-
-// Константы нельзя объявлять с использованием синтаксиса :=.
+func main() {
+	fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
+}
+// 21
+// 0.2
+// 1.2676506002282295e+29
